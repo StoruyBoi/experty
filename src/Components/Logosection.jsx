dbe647a3e5fe9e67af3e77be5b68c9445bcd6750
itsjasminder <trepan.chd8427@gmail.com>
2023-10-24 10:10:43 +0530

    h

--- src/Components/Logosection.jsx
+++ src/Components/Logosection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Style/Logosection.css";
+
 import amazon from "../source/amazon.png";
 import google from "../source/google.png";
 import altassian from "../source/atlassian.png";
